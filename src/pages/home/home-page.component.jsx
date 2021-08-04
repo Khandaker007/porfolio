2318c90dfaa5d7d82efaf00dee3479b5ba5148b2
ninad <na.ninad60@gmail.com>
2021-08-05 04:58:25 +0600

    complete hero section and added my image file

--- src/pages/home/home-page.component.jsx
+++ src/pages/home/home-page.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+// IMAGE
+import myPhoto from '../../asset/image/Khandaker.jpg'
+
 import './home-page.style.scss'
+import '../../sass/typography.scss'
 
 const HomePage = () => (
     <div className='home-page'>
         <section className='hero'>
-            hero section 
+            <div className="hero__img-container">
+                <img className='hero__img' src={myPhoto} alt="person image"/>
+            </div>
+            <div className="hero__content">
+                <h1 className="heading-1">I'm Khandaker Nabiul.</h1>
+                <h1 className="heading-1">A Front-end Developer</h1>
+                <h3 className='heading-3'>based in Bangladesh</h3>
+                <p className='text'>It is a long established fact that a reader will be distracted by the readable<br/> fact that a reader will be distracted by the readable content.</p>
+            </div>
         </section>
         <div className="cv-content">
             cv content
