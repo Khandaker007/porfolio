1f0910ed086b60f8482499879c462851793024e4
ninad <na.ninad60@gmail.com>
2021-08-04 18:00:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+// COMPONENTS
+import HomePage from './pages/home/home-page.component'
+
+import './App.scss'
+
+function App() {
+
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/home-page.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import './home-page.style.scss'
+
+const HomePage = () => (
+    <div className='home-page'>
+        <section className='hero'>
+            hero section 
+        </section>
+        <div className="cv-content">
+            cv content
+        </div>
+        <div className="cv-download">
+            cv download
+        </div>
+        <div className="philosophy-content">
+            philosophy content
+        </div>
+        <div className="philosophy-image">
+            philosophy image
+        </div>
+        <div className="skills-content">
+            skill content
+        </div>
+        <div className="skills-bar">
+            skills bar
+        </div>
+        <section className='my-work'>
+            my work
+        </section>
+        <div className="github-content">
+            github content
+        </div>
+        <div className="github-image">
+            github image
+        </div>
+        <section className='upwork-content'>
+            upwork
+        </section>
+        <div className="upwork-image">
+            upwork image
+        </div>
+        <div className="contact-content">
+            contact content
+        </div>
+        <div className="contact-form">
+            contact form
+        </div>
+        <footer className='footer'>
+            footer
+        </footer>
+    </div>
+)
+
+export default HomePage;
